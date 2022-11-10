6e89f267f44e19b9a14f0b80fb3a54ff39f71bcf
Anuth Hatharasinghe <anuth.hatharasinghe@gmail.com>
2022-11-10 22:31:43 +1100

    Guessing game challenge completed with modification for number between 1-100

--- while_loop_exercise.py
+++ while_loop_exercise.py
@@ -4,10 +4,6 @@ print('Guessing game')
 # Guess the correct number in 3 guesses. If you don’t get it right after 3 guesses you lose the game.
 # Give user input box: 1. To capture guesses,
 # print(and input boxes) 1. If user wins 2. If user loses
-# Tip:( remember you won’t see  print statements durng execution, so If you want to see prints during whle loop, then print to the input box
-
-#Modification 1: number 1-100, tell user if guess is too high/low ,and let them have 5-10 guesses.
-# Tip:( remember you won’t see  print statements during execution, so If you want to see prints during whle loop, print to the input box (This is specific to this platform)
 # Three Loop Questions:
 #1. What do I want to repeat?
 #  ->
@@ -31,3 +27,30 @@ while i < guesses:
         print(f'You have {guesses-(i)} guesses left!')
         if i != guesses:
             user_guess = input("Try again: ")
+        else:
+            print(f'The number was {random_number}!!!')
+
+#Modification 1: number 1-100, tell user if guess is too high/low ,and let them have 5-10 guesses.
+print('\nGuessing Game 2 (HARD)')
+
+guesses = 10
+user_guess = input("Please enter your guess between 1-100: ")
+random_number = random.randrange(1, 100)
+i = 0
+
+while i < guesses:
+    i+=1
+    if int(user_guess) == random_number:
+        print('You guessed right!')
+        break
+    else:
+        if int(user_guess) < random_number:
+            print('Your guess is too Low!')
+        else:
+            print('Your guess is too High!')
+
+        print(f'You have {guesses-(i)} guesses left!')
+        if i != guesses:
+            user_guess = input("Try again: ")
+        else:
+            print(f'The Correct number was {random_number}!!!')
