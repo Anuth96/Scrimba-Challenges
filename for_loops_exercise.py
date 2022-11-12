ad6fdedf4cee710974bc634c9eaf26164475b0ea
Anuth Hatharasinghe <anuth.hatharasinghe@gmail.com>
2022-11-13 09:25:13 +1100

    Challenge completed

--- for_loops_exercise.py
+++ for_loops_exercise.py
@@ -1,2 +1,14 @@
 names = ['john ClEEse','Eric IDLE','michael']
 names1 = ['graHam chapman', 'TERRY', 'terry jones']
+#First lets combine these two lists
+names.extend(names1)
+#Then ask for user input if they would like to add any new friends
+user_input = input('Would you like to add any new Friends to your invitation list? (Y/N)').strip()
+while user_input.lower() == 'y':
+    names.append(input('Please enter your Friends full name: ').strip())
+    user_input = input('Would you like to add another? (Y/N)').strip()
+else:
+    print('\nOkay here are your invitations......')
+#Now lets create a for loop for each invitation
+for element in names:
+    print(f'{element.title()}! You are invited to the party on Saturday.')
