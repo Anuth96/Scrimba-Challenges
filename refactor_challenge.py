5fd52b478422ec9a14503b6d84abdfc43e0d1a06
Anuth Hatharasinghe <anuth.hatharasinghe@gmail.com>
2022-11-12 20:03:32 +1100

    fixed code, conditional error

--- refactor_challenge.py
+++ refactor_challenge.py
@@ -31,7 +31,7 @@
 
 def num_days(month):
 
-    if month == 'jan' or 'mar' or 'may' or 'jul' or 'aug' or 'oct' or 'nov' or 'dec':
+    if month == 'jan' or month == 'mar' or month == 'may' or month == 'jul' or month == 'aug' or month == 'oct' or month == 'nov' or month == 'dec':
         print('number of days in',month,'is',31)
     elif month == 'feb':
         print('number of days in',month,'is',28)
